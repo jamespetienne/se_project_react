25ac5a2a402ea347fc1d98a8a84bc02f45630b76
James Etienne <jamespetienne@gmail.com>
2024-08-18 22:49:03 -0400

    Prject 11

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,61 +1,27 @@
-// import "./ItemModal.css";
-// import close from "../../assets/close-white.png";
-
-// function ItemModal({ isOpened, selectedCard, handleCloseClick }) {
-//   return (
-//     <div className={`modal ${isOpened && "modal_opened"}`}>
-//       <div className="modal__container modal__content_type_image">
-//         <button className="modal__close" type="button">
-//           <img
-//             src={close}
-//             alt="close-button"
-//             className="modal__close-image"
-//             onClick={handleCloseClick}
-//           />
-//         </button>
-//         <img
-//           src={selectedCard.link}
-//           alt={selectedCard.name}
-//           className="modal__card_image"
-//         />
-//         <div className="modal__footer">
-//           <h2 className="modal__card_name">{selectedCard.name}</h2>
-//           <p className="modal__card_weather">Weather: {selectedCard.weather}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ItemModal;
-
 import "./ItemModal.css";
-import closeBtn from "../../assets/cls-btn.svg";
+import close from "../../assets/close-white.png";
 
-function ItemModal({ activeModal, handleCloseClick, card, openDeleteModal }) {
+function ItemModal({ isOpened, selectedCard, handleCloseClick }) {
   return (
-    <div className={`modal ${activeModal === "preview" ? "modal_opened" : ""}`}>
-      <div className="modal__content modal__content_type_image">
-        <button
-          onClick={handleCloseClick}
-          type="button"
-          className="modal__close"
-        >
-          <img src={closeBtn} alt="close button" className="modal__close-btn" />
+    <div className={`modal ${isOpened && "modal_opened"}`}>
+      <div className="modal__container modal__content_type_image">
+        <button className="modal__close" type="button">
+          <img
+            src={close}
+            alt="close-button"
+            className="modal__close-image"
+            onClick={handleCloseClick}
+          />
         </button>
-        <img src={card.imageUrl} alt="Image" className="modal__image" />
+        <img
+          src={selectedCard.link}
+          alt={selectedCard.name}
+          className="modal__card_image"
+        />
         <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
-          <p className="modal__weather">Weather: {card.weather}</p>
+          <h2 className="modal__card_name">{selectedCard.name}</h2>
+          <p className="modal__card_weather">Weather: {selectedCard.weather}</p>
         </div>
-        <button
-          onClick={() => {
-            openDeleteModal();
-          }}
-          className="card-item__delete-btn"
-        >
-          Delete Item
-        </button>
       </div>
     </div>
   );
